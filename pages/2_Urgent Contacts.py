9ef0ada100ac20f4c7f2e6ce4a02ddbbfdc2475a
Barny <barnabyrumbold@hotmail.com>
2026-03-04 09:04:47 +0000

    updated app

--- pages/2_Urgent Contacts.py
+++ pages/2_Urgent Contacts.py
@@ -4,16 +4,20 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 
-THEME_COLOUR = "#707275"
+THEME_COLOUR = "#597AAC"
 
 path = r"C:\Users\barnyrumbold\OneDrive - Kidney Research UK\Desktop\Hackathon\Northstar-Desk-Decision-Support-Tool-\Data\clean.csv"
 data = pd.read_csv(path)
 
-st.markdown(f"<h1 style='color:{THEME_COLOUR};'>Hackathon</h1>", unsafe_allow_html=True)
+st.markdown(f"<h1 style='color:{THEME_COLOUR};'>Hackathon - Top 10 Urgent Contacts</h1>", unsafe_allow_html=True)
 st.write('This page provides a list of those enquiriers that require urgent contact.') 
 
 top10 = data.sort_values("priority", ascending=False).head(10)
 
-st.markdown(f"<h2 style='color:{THEME_COLOUR};'>Top 10 Urgent Contacts</h2>", unsafe_allow_html=True)
+
 st.dataframe(top10)
 st.table()
+st.markdown(
+    "[View Project on GitHub](https://github.com/OzzyHF/Northstar-Desk-Decision-Support-Tool-)",
+    unsafe_allow_html=True
+)
--- pages/3_Model Card.py
+++ pages/3_Model Card.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 
-THEME_COLOUR = "#707275"
+THEME_COLOUR = "#597AAC"
 
 
-st.markdown(f"<h1 style='color:{THEME_COLOUR};'>Hackathon</h1>", unsafe_allow_html=True)
-st.markdown(f"<h2 style='color:{THEME_COLOUR};'>Model Card</h2>", unsafe_allow_html=True)
-st.write('This page provides key model information from our model')
+st.markdown(f"<h1 style='color:{THEME_COLOUR};'>Hackathon - Model Card</h1>", unsafe_allow_html=True)
+
+st.write('This page provides key information about our model')
 
 model_type = 'Logistic Regression'
 mse = 0.00
@@ -21,3 +21,7 @@ col2.metric("Mean Squared Error", f"{mse:.1f}%")
 col3.metric("Mean Absolute Error", f"{mae:.1f}%")
 col4.metric("R2", f"{r2:.1f}%")
 
+st.markdown(
+    "[View Project on GitHub](https://github.com/OzzyHF/Northstar-Desk-Decision-Support-Tool-)",
+    unsafe_allow_html=True
+)
